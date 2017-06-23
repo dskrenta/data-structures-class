38a8822edea8a3255ddf26b2c0607d8a2d363a05
dskrenta <dskrenta@gmail.com>
2017-06-22 19:34:50 -0700

    legacy data structures code

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,26 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+let thing = "who are you?"
+
+print(thing)
+
+print("\(str) you are funny... \(thing)")
+
+let arr = [1, 3, 4, 5, 6, 7, 8]
+
+let stuff = [
+    1: "thing",
+    2: "stuff"
+]
+
+print(arr[5])
+print(stuff[2])
+
+for number in arr {
+    print(number)
+}
+
